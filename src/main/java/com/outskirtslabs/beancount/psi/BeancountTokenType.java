f3d699fd8c6b97b349653307d222f8c77097fa3a
Niko Strijbol <strijbol.niko@gmail.com>
2020-12-09 22:22:49 +0100

    Implement spell & grammar checking

--- src/main/java/com/outskirtslabs/beancount/psi/BeancountTokenType.java
+++ src/main/java/com/outskirtslabs/beancount/psi/BeancountTokenType.java
@@ -1,17 +1,13 @@
 package com.outskirtslabs.beancount.psi;
 
+import com.intellij.psi.tree.IElementType;
+import com.outskirtslabs.beancount.BeancountLanguage;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import com.intellij.psi.tree.IElementType;
-import com.outskirtslabs.beancount.BeancountLanguage;
-
 public class BeancountTokenType
-    extends IElementType
-
-{
-    public BeancountTokenType(@NotNull @NonNls String debugName)
-    {
+        extends IElementType {
+    public BeancountTokenType(@NotNull @NonNls String debugName) {
         super(debugName, BeancountLanguage.INSTANCE);
     }
 }
