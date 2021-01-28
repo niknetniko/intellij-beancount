745acf5caadd9c4d518cd702513739218b923404
Niko Strijbol <strijbol.niko@gmail.com>
2021-01-29 00:01:58 +0100

    Improved contributor

--- src/main/java/com/outskirtslabs/beancount/navigation/GoToAccountSymbol.java
+++ src/main/java/com/outskirtslabs/beancount/navigation/GoToAccountSymbol.java
@@ -1,22 +1,28 @@
 package com.outskirtslabs.beancount.navigation;
 
 import com.intellij.navigation.ChooseByNameContributor;
+import com.intellij.navigation.ChooseByNameContributorEx;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.Processor;
+import com.intellij.util.indexing.FindSymbolParameters;
+import com.intellij.util.indexing.IdFilter;
 import com.outskirtslabs.beancount.psi.stub.index.AccountStubIndex;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Niko Strijbol
  */
-public class GoToAccountSymbol implements ChooseByNameContributor {
+public class GoToAccountSymbol implements ChooseByNameContributorEx {
     @Override
-    public String @NotNull [] getNames(Project project, boolean includeNonProjectItems) {
-        return AccountStubIndex.findAllAccounts(project).toArray(String[]::new);
+    public void processNames(@NotNull Processor<? super String> processor, @NotNull GlobalSearchScope scope, @Nullable IdFilter filter) {
+        AccountStubIndex.findAllAccounts(scope.getProject()).forEach(processor::process);
     }
 
     @Override
-    public NavigationItem @NotNull [] getItemsByName(String name, String pattern, Project project, boolean includeNonProjectItems) {
-        return AccountStubIndex.find(project, name).toArray(NavigationItem[]::new);
+    public void processElementsWithName(@NotNull String name, @NotNull Processor<? super NavigationItem> processor, @NotNull FindSymbolParameters parameters) {
+        AccountStubIndex.find(parameters.getProject(), name).forEach(processor::process);
     }
 }
