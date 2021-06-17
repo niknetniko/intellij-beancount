9d888e9850304588c1bc97dbb10dafd18a8ec0f8
Niko Strijbol <strijbol.niko@gmail.com>
2021-06-17 23:22:35 +0200

    Experimental change to disable restartable lexer
    Fixes #2

--- src/main/java/com/outskirtslabs/beancount/parser/LexerAdapter.java
+++ src/main/java/com/outskirtslabs/beancount/parser/LexerAdapter.java
@@ -1,9 +1,27 @@
 package com.outskirtslabs.beancount.parser;
 
 import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.RestartableLexer;
+import com.intellij.lexer.TokenIterator;
+import org.jetbrains.annotations.NotNull;
 
-public class LexerAdapter extends FlexAdapter {
+public class LexerAdapter extends FlexAdapter implements RestartableLexer {
     public LexerAdapter() {
         super(new BeancountLexer(null));
+    }
+
+    @Override
+    public int getStartState() {
+        return 0;
+    }
+
+    @Override
+    public boolean isRestartableState(int state) {
+        return false;
+    }
+
+    @Override
+    public void start(@NotNull CharSequence buffer, int startOffset, int endOffset, int initialState, TokenIterator tokenIterator) {
+        this.start(buffer, startOffset, endOffset, initialState);
     }
 }
